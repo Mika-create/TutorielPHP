93a29cba0b94857ff34eceb801e3e2afc9f34df6
Mikaël Moreau <83815495+Mika-create@users.noreply.github.com>
2024-01-27 16:25:47 +0100

    Add files via upload

--- demo4.php
+++ demo4.php
@@ -57,3 +57,46 @@ On demande à l'utilisateur de rentrer une heure et on lui dira si le magasin es
    // On demande si on veut rajouter un nouveau créneau (o/n)
 //On demande à l'utilisateur de rentrer une heure
 //On affiche l'état d'ouverture du magasin
+
+$creneaux = [
+];
+
+while (true) {
+   $debut = (int)readline('Heure d\'ouverture : ');
+   $fin = (int)readline('Heure de fermeture : ');
+   if ($debut >= $fin) {
+       echo 'Le créneaux ne peut pas être enregistré car l\'heure de début ($debut) est supérieur à l\'heure de fin' ;
+   } else {
+        $creneaux[] = [$debut, $fin];
+        $action = readline('Entrer un nouveau créneau (n)');
+        if ($action === 'n') {
+            break;
+        }
+   }
+} 
+
+// Le magasin est ouvert de 14h à 18h et de 9h 12h
+
+echo 'Le magasin est ouvert de ';
+foreach($creneaux as $k => $creneau) {
+    if ($k > 0) {
+        echo ' et de ';
+    }
+    echo "{$creneau[0]}h à {$creneau[1]}h";
+}
+
+// $heure = (int)readline("A quelle heure voulez vous visiter le magasin?");
+// $creneauTrouve = false;
+
+// foreach($creneaux as $creneau) {
+//     if ($heure >= $creneau[0] && $heure <= $creneau[1]){
+//         $creneauTrouve = true;
+//         break;
+//     }
+// }
+
+// if ($creneauTrouve) {
+//     echo 'Le magasin sera ouvert';
+// } else {
+//     echo 'Désolé, le magasin sera fermé :(';
+// }
