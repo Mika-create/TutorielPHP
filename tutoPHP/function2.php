db34079ad3bd1bdeac2ef33af5f7daca11c98398
Mikaël Moreau <83815495+Mika-create@users.noreply.github.com>
2024-02-06 17:04:18 +0100

    Add files via upload

--- tutoPHP/function2.php
+++ tutoPHP/function2.php
@@ -1,4 +1,9 @@
 <?php
+
+//declare(strict_types=1); //évites les conversions implicites
+
+//Création d'une fonction
+
 // $nom = "Doe";
 
 // function bonjour ($prenom = null) {
@@ -10,4 +15,50 @@
 // }
 
 // echo bonjour('Jean');
+
+ function repondre_oui_non (?string $phrase = null): bool {
+     while (true) {
+     $reponse = readline($phrase . " - (o)ui/(n)on : ");
+      if ($reponse === "o") {
+         return true;
+      }elseif ($reponse === 'n') {
+         return false;
+     }
+     }
+ }
+
+function demander_creneau ($phrase = 'Veuillez entrer un créneau') {
+    echo $phrase . "\n";
+    while (true){
+        $ouverture = readline('Heure d\'ouverture : ');
+        if ($ouverture >= 0 || $ouverture <= 23) {
+            break;
+        }
+    }
+    while (true) {
+        $fermeture = (int)readline('Heure de fermeture : ');
+        if ($fermeture >= 0 && $fermeture <= 23 && $fermeture > $ouverture){
+            break;
+        }
+    }
+    return [$ouverture, $fermeture];
+}
+
+// //$resultat = repondre_oui_non('Voulez vous continuer ?');
+// $creneau = demander_creneau();
+// $creneau2 = demander_creneau('Veuillez entrer votre creneau');
+// //si l'utilisateur tape "o" => true
+// //si l'utilisateur tape "n" => false
+// var_dump($creneau, $creneau2);
+
+function demander_creneaux (string $phrase = "Veuillez entre vos creéneaux"): array{
+    $creneaux = [];
+    $continuer = true;
+    while ($continuer){
+        $creneau[] = demander_creneau();
+        $continuer = repondre_oui_non('Voulez vous continuer ? ');
+    }
+    return $creneaux;
+}
+
 
