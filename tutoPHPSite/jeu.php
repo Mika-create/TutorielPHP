54fc9d23c6d34c2287cca2b58ac1da5617bbb754
Mikaël Moreau <83815495+Mika-create@users.noreply.github.com>
2024-02-23 14:49:55 +0100

    Add files via upload

--- tutoPHPSite/jeu.php
+++ tutoPHPSite/jeu.php
@@ -1,17 +1,36 @@
 <?php
+//Checkbox
+$parfums = [
+    'Fraise' => 4,
+    'Chocolat' => 5,
+    'Vanille' => 3
+];
+//Radio
+$cornets = [
+    'Pot' => 2,
+    'Cornet' => 3
+];
+//Checkbox
+$supplements = [
+    'Pépites de chocolat' => 1,
+    'Chantilly' => 0.5
+];
+$title = "Composer votre glace";
+
+
 $aDeviner = 150;
 $erreur = null;
 $succes = null;
 $value = null;
-if (isset($_GET['chiffre'])) {
-    if ($_GET['chiffre'] > $aDeviner) {
+if (isset($_POST['chiffre'])) {
+    $value = (int)$_POST['chiffre'];
+    if ($value> $aDeviner) {
         $erreur = "Votre chiffre est trop grand";
-    } elseif ($_GET['chiffre'] < $aDeviner) {
+    } elseif ($value < $aDeviner) {
         $erreur = "Votre chiffre est trop petit";
     } else {
         $succes = "Bravo! Vous avez devinez le chiffre <strong>$aDeviner</strong>";
     }
-    $value = (int)$_GET['chiffre'];
 }
 require 'header.php';
 ?>
@@ -26,9 +45,21 @@ require 'header.php';
     </div>
 <?php endif ?>    
 
+<h1>Composer votre glace</h1>
 
 <form action="/jeu.php" method="POST">
     <div class="form-group">
+
+        <?php foreach($parfums as $parfum => $prix): ?>
+            <div class="checkbox">
+                <label>
+                    <input type="checkbox" name="parfum[]" value="<?= $parfum ?>">
+                    <?= $parfum ?> - <?= $prix ?> €
+                </label>
+            </div>
+        <?php endforeach; ?>
+        <button type="submit" class="btn btn-primary">Composer ma glace</button>   
+
        <input type="number" class="form-control" name="chiffre" placeholder="entre 0 et 1000" value="<?= $value ?>">
     </div> 
     <button type="submit" class="btn btn-primary">Deviner</button>
